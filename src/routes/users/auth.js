8f2614bafcb46922832dc90e3e6d1458c75a522b
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-01-20 21:42:13 +0100

    user-auth

---
+++ src/routes/users/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const UserSchema = require("./Schema");
+
+const authenticate = async (user) => {
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "15000",
+  });
+  const refreshToken = jwt.sign(
+    { _id: user._id },
+    process.env.REFRESH_JWT_KEY,
+    {
+      expiresIn: "1 week",
+    }
+  );
+  user.refresh_tokens = user.refresh_tokens.concat(refreshToken);
+  await UserSchema.update(
+    { refresh_tokens: user.refresh_tokens },
+    { where: { _id: user._id } }
+  );
+  return { user, token, refreshToken };
+};
+
+module.exports = { authenticate };
